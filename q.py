4907e7452b934c490757a204afaf41ef9f7ffca7
RyanDuncan14 <111906403+RyanDuncan14@users.noreply.github.com>
2026-02-26 20:40:55 -0500

    Add files via upload

---
+++ q.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+        
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.size = 0 
+        
+    def pop_left(self):
+        if not self.head:
+            return None
+        current_head = self.head
+        self.size -= 1 
+        return current_head
+    
+    def add(self,item):
+        new_node = Node(item)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                if not current.next:
+                    break
+                current = current.next
+            current.next = new_node
+        self.size += 1
+
+    def is_empty(self):
+        if self.head is None:
+            return False
+        else:
+            return True  
+    
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+    
+    def __repr__(self):
+        return_str = "Queue object: "
+        for node in self:
+            return_str = return_str + node.data + "--"
+            return return_str
+        
+    def __str__(self):
+        return_str = "Queue object: "
+        for node in self:
+            return_str = return_str + node.data + "--"
+        return_str += self.head 
+        return return_str     
